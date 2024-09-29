9bee1f103563f0f8ea8c79f56e1a3bf466662e7e
pradipawasthi <pradipawasthi701@gmail.com>
2024-09-29 15:23:21 +0530

    [WIP] : added card swipe like tinder

---
+++ PokiCards/Views/TinderStackView.swift
@@ -0,0 +1,18 @@
+//
+//  TinderStackView.swift
+//  PokiCards
+//
+//  Created by Pradip Awasthi on 29/09/24.
+//
+
+import SwiftUI
+
+struct TinderStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TinderStackView()
+}
